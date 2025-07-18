a75fde94a11ee820955ec8d1dac0b82654d32dfe
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-18 11:04:49 +0200

    Delete src directory

--- src/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('MONGO_URI', 'sua-uri-mongodb-atlas');
-define('MONGO_DB',  'phil-asean');
-// Se precisar de token para criar admin:
-define('ADMIN_TOKEN', 'um-token-muito-secreto');
--- src/public/register.php/create_order.php
+++
@@ -1,7 +0,0 @@
-<?php
-require __DIR__ . '/../bootstrap.php';
-use App\Controllers\OrderController;
-
-$input = json_decode(file_get_contents('php://input'), true);
-$ctrl  = new OrderController();
-echo json_encode($ctrl->create($input));
