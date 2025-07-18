659af8cddc88dc662a0cbfed582928f597b1af30
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-18 23:43:39 +0200

    Update auth.routes.js

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
-const { register, login } = require('../controllers/auth.controller');
-router.post('/register', register);
-router.post('/login',    login);
+const { register, login, registerAdmin } = require('../controllers/auth.controller');
+
+router.post('/register',       register);
+router.post('/login',          login);
+// rota aberta apenas para criar seu primeiro admin
+router.post('/register-admin', registerAdmin);
+
 module.exports = router;
