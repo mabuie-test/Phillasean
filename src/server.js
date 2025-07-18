677ad3bc375012da0913d7d7c8b9f05fb9e7edf8
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-18 11:16:16 +0200

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors    = require('cors');
+const connectDB = require('./config/db');
+
+const authRoutes  = require('./routes/auth.routes');
+const orderRoutes = require('./routes/order.routes');
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+app.use('/public', express.static('invoices'));
+
+app.use('/api/auth', authRoutes);
+app.use('/api',      orderRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`API rodando na porta ${process.env.PORT}`);
+});
