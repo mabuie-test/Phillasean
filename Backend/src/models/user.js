01a41e26449aee859e6a398ca21114d421f429b5
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-18 23:43:03 +0200

    Update user.js

--- Backend/src/models/user.js
+++ Backend/src/models/user.js
@@ -4,15 +4,17 @@ const bcrypt = require('bcrypt');
 const userSchema = new Schema({
   name:     { type: String, required: true },
   email:    { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  role:     { type: String, enum: ['user','admin'], default: 'user' }
 });
 
-// hash antes de salvar
+// Hash da senha antes de salvar
 userSchema.pre('save', async function() {
   if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 10);
 });
 
+// Método para comparar senha
 userSchema.methods.comparePassword = function(plain) {
   return bcrypt.compare(plain, this.password);
 };
