0ed22541342703a6a88f4e31970e2186b07d7019
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-20 15:16:48 +0200

    Update Order.js

--- Serv/models/Order.js
+++ Serv/models/Order.js
@@ -3,11 +3,11 @@ const { Schema, model, Types } = require('mongoose');
 const OrderSchema = new Schema({
   client:    { type: Types.ObjectId, ref: 'User', required: true },
   details: {
-    services:      [{ type: String }],    // agora é array de strings
-    notes:         String,
-    vessel:        String,
-    port:          String,
-    estimatedDate: Date
+    services:      req.body.services || [],
+      notes:         req.body.notes,
+      vessel:        req.body.vessel,
+      port:          req.body.port,
+      estimatedDate: req.body.date
   },
   status:    { type: String, enum: ['pending','in_progress','completed'], default: 'pending' },
   createdAt: { type: Date, default: Date.now }
