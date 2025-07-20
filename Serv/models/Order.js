4c5ead4487902c97204156fae34a38734ca81464
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-20 13:48:21 +0200

    Update Order.js

--- Serv/models/Order.js
+++ Serv/models/Order.js
@@ -7,7 +7,8 @@ const OrderSchema = new Schema({
     notes:         String,
     vessel:        String,
     port:          String,
-    estimatedDate: Date
+    estimatedDate: Date,
+    phone:         String
   },
   status:    { type: String, enum: ['pending','in_progress','completed'], default: 'pending' },
   createdAt: { type: Date, default: Date.now }
