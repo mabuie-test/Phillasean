0e4f0367546735cb9530041040caf24bc24ddd01
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-21 09:09:41 +0200

    Update admin.js

--- Finalfrontend/src/routes/admin.js
+++ Finalfrontend/src/routes/admin.js
@@ -2,13 +2,13 @@
 import express from 'express';
 import { listInvoices, createAdmin, getAuditLogs } from '../controllers/adminController.js';
 // Removido temporariamente: autenticação e autorização
-// import { authMiddleware } from '../middlewares/auth.js';
-// import { roleMiddleware } from '../middlewares/roles.js';
+import { authMiddleware } from '../middlewares/auth.js';
+import { roleMiddleware } from '../middlewares/roles.js';
 
 const router = express.Router();
 
 // Descomente estas linhas para reabilitar proteção:
-// router.use(authMiddleware, roleMiddleware('admin'));
+ router.use(authMiddleware, roleMiddleware('admin'));
 
 router.get('/invoices', listInvoices);
 router.post('/admins',   createAdmin);
