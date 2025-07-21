f04d8413bb9fa6983a630c7ebd6dbc96888a3ffa
JorgeMabuie <jorgemaabuie@gmail.com>
2025-07-21 11:49:19 +0200

    Create auth.js

---
+++ phil-backend/src/routes/auth.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import { register, login } from '../controllers/authController.js';
+const r = express.Router();
+r.post('/register', register);
+r.post('/login',    login);
+export default r;
